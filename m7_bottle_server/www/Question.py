f1fe941d63e5fdaffdee2b25dd7b6bbc2fdab142
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2021-03-08 12:56:53 +0100

    Strapi setup

---
+++ m7_bottle_server/www/Question.py
@@ -0,0 +1,35 @@
+import json
+import requests as request
+#interface 
+class Question:
+    def __init__(self):
+        self.api_url = "http://localhost:1337"
+
+    def all(self):
+        r = request.get(self.api_url + "/questions")
+        return r.json()
+    
+
+    #CRUD
+    def create(self, jwt, params):
+        token = f'Bearer {jwt}'
+        endpoint = self.api_url + "/questions"
+        data = json.dumps({
+            'title': params["title"],
+            'question': params["question"]
+        })
+        headers = {'Content-Type': 'application/json', 'Authorization': token}
+        r = request.post(endpoint,data=data, headers=headers)
+        return r
+
+    def read(self):
+        endpoint = self.api_url + "/questions"
+        headers = {'Content-Type': 'application/json'}
+        r = request.get(endpoint, headers=headers)
+        return r
+
+    def update(self): 
+        pass
+    def delete(self):
+        pass
+
