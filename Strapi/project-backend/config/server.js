4b43e764d32128c8cb02ee0e85ba2a8f7b1000b3
Holthuizen <45522614+Holthuizen@users.noreply.github.com>
2021-03-08 10:16:08 +0100

    bottle, strapi

---
+++ Strapi/project-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0898875ffd9e82ef3238e67955549b2f'),
+    },
+  },
+});
---
+++ Strapi/project-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1561f5d2-7bdc-4c6c-a010-0c3106fbd399'
+};
